f8ab0f887479f1fa1befbe1603f6a25f4728ac67
blrxbadboy <rayenbounawara7@gmail.com>
2026-01-13 19:06:11 +0100

    problem

--- probleme1.js
+++ probleme1.js
@@ -1,7 +1,11 @@
 // 1-Créer une fonction qui combine ton prénom et ton nom et retourne le résultat avec un espace entre les deux.
 
 
-
+function combin() {
+    return "med rayen" + " " + "bounawara"
+    
+}
+combin()
 
 
 
@@ -10,6 +14,10 @@
 
 // 2-Écris une fonction qui prend deux nombres et retourne la moyenne.
 
+function moyenne(n1,n2) {
+    return (n1+n2)/2
+    
+}
 
 
 
@@ -20,7 +28,10 @@
 
 // 3-Crée une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu'elle contient.
 
-
+function nchaine(ch) {
+    return ch.length
+    
+}
 
 
 
@@ -31,7 +42,10 @@
 // 4-Crée une fonction qui retourne la sous-chaîne à partir d'une position donnée jusqu'à la fin.
 
 
-
+function souschaine(ch,pos) {
+    return ch.substr(pos)
+    
+}
 
 
 
