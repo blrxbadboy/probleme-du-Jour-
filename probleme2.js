0120de89d68641ffc67583522a1ac91f96452390
blrxbadboy <rayenbounawara7@gmail.com>
2026-01-14 19:06:25 +0100

    v3

--- probleme2.js
+++ probleme2.js
@@ -55,7 +55,7 @@ function PG(n1,n2) {
 // 4- Crée une fonction qui prend une note (0 à 20) et retourne "réussi" si la note est >= 10, sinon "échoué".
 
 function note(n) {
-    if (n>=10) {
+    if (n>=10 && n<=20) {
         return "reussi"
         
     }
